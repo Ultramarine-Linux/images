f79270934668b7e93f218fcd72674d2b60f605e1
korewaChino <crkza1134@gmail.com>
2021-08-05 21:32:38 +0700

    changed name

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ksflatten --config kickstarts/ultramarine-cyber.ks --output flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
-livecd-creator --verbose --config flattened.ks --fslabel Ultramarine-Cyber --cache tmp/cache/
+livecd-creator --verbose --config flattened.ks --fslabel "Ultramarine-Linux" --cache tmp/cache/
 rm -rf tmp/
 EOF

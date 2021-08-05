169ee1878164e7d43005837c99272b2b710bfcbd
korewaChino <crkza1134@gmail.com>
2021-08-05 22:35:00 +0700

    tee build logs

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ksflatten --config kickstarts/ultramarine-cyber.ks --output flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
-livecd-creator --verbose --config flattened.ks --fslabel "Ultramarine-Linux" --cache tmp/cache/
+livecd-creator --verbose --config flattened.ks --fslabel "Ultramarine-Linux" --cache tmp/cache/ | tee build.log
 rm -rf tmp/
 EOF

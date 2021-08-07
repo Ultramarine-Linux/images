8019682f6164ee1c215f4efea1ebab150ff47b57
korewaChino <crkza1134@gmail.com>
2021-08-08 03:19:37 +0700

    full revert to original build script, now improved

--- build.sh
+++ build.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-kickstartFile="${1}"
-project="\"${2}\""
-livemediaFsLabel="${3}"
-
 sudo -s <<<"
 echo Flattening Scripts...
-ksflatten --config ${kickstartFile} --output flattened.ks
+ksflatten --config kickstarts/ultramarine-cyber.ks --output flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
 rm -rf build/
-livemedia-creator\
- --make-iso\
- --ks flattened.ks\
- --nomacboot\
- --project ${project}\
- --fs-label ${livemediaFsLabel}\
- --location lorax/\
- --iso /files/f34-base.iso\
- --no-virt\
- --iso-only\
- --resultdir build/
+livecd-creator flattened.ks -t tmp/ -f Ultramarine-Linux-Live
 rm -rf tmp/
 "

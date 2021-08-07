7547f62226e5378cdd2091c962a5ff4ebe21795d
korewaChino <crkza1134@gmail.com>
2021-08-08 03:48:53 +0700

    using LZO compression for SPID

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+spin="${1}"
 sudo -s <<<"
 echo Flattening Scripts...
-ksflatten --config kickstarts/ultramarine-cyber.ks --output flattened.ks
+ksflatten --config kickstarts/ultramarine-${spin}.ks --output flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
 rm -rf build/
-livecd-creator flattened.ks -t tmp/ -f Ultramarine-Linux-Live
+livecd-creator flattened.ks -t tmp/ --compression-type lzo -f Ultramarine-Linux-Live
 rm -rf tmp/
 "

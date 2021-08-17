1fefb10b2a1cecd1ec9dd072a0505c2d828eed7f
korewaChino <crkza1134@gmail.com>
2021-08-17 15:15:03 +0700

    make deletions more specific

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ spin="${1?"Usage: $0 Spin"}"
 
 
 rm -f flattened.ks
-rm -f -- *.iso
+rm -f -- "Ultramarine-Linux-Live-${spin}".iso
 sudo -s <<<"
 echo Flattening Scripts...
-ksflatten --config kickstarts/ultramarine-${spin}.ks --output flattened.ks && sed -i 's/\r$//' flattened.ks
+ksflatten --config "kickstarts/ultramarine-${spin}.ks" --output flattened.ks && sed -i 's/\r$//' flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
@@ -15,8 +15,9 @@ livecd-creator flattened.ks\
  -v\
  -d\
  --compression-type zstd\
- -f Ultramarine-Linux-Live\
+ -f "Ultramarine-Linux-Live-${spin}"\
  --title 'Ultramarine Linux 34'\
  --product 'Ultramarine Linux'
+
 rm -rf tmp/
 "

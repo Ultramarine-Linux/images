f7fca16f5dd936374a477cad1ca33f90d8ac9b78
korewaChino <crkza1134@gmail.com>
2021-08-18 11:25:38 +0700

    rename files to accomodate for the LBS

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ spin="${1?"Usage: $0 Spin"}"
 
 sudo -s <<<"
 rm -f flattened.ks
-rm -f -- "Ultramarine-Linux-Live-${spin}".iso
+rm -f -- Ultramarine-Linux-Live-${spin}.iso
 echo Flattening Scripts...
-ksflatten --config kickstarts/ultramarine-"${spin}".ks --output flattened.ks && sed -i 's/\r$//' flattened.ks
+ksflatten --config kickstarts/ultramarine-${spin}.ks --output flattened.ks && sed -i 's/\r$//' flattened.ks
 echo ------------------------------------------------------- 
 echo --------------------BUILDING ISO-----------------------
 echo -------------------------------------------------------
@@ -15,11 +15,11 @@ rm -rf build/
 livecd-creator flattened.ks\
  -v\
  --compression-type zstd\
- -f Ultramarine-Linux-Live-"${spin}"\
+ -f Ultramarine-Linux-Live-${spin}\
  --title 'Ultramarine Linux'\
  --product 'Ultramarine Linux'
-
+mv Ultramarine-Linux-Live-${spin}.iso ${spin}.iso
 echo Cleaning up...
 rm -rf /var/tmp/imgcreate* > /dev/null
-#rm -rf /tmp/lapis
+rm -rf /tmp/lapis
 "

634e6c152e56e4605436c547fe9f55178360e5a2
Cappy Ishihara <cappy@cappuchino.xyz>
2023-10-19 01:47:20 +0700

    initial katsu setup

---
+++ katsu/modules/flagship/flagship-live.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+systemctl disable gdm
+
+# set livesys session type
+sed -i 's/^livesys_session=.*/livesys_session="budgie-desktop"/' /etc/sysconfig/livesys
+
+cat >> /var/lib/livesys/livesys-session-extra << EOF
+## set up lightdm autologin
+sed -i 's/^#autologin-user=.*/autologin-user=liveuser/' /etc/lightdm/lightdm.conf
+sed -i 's/^#autologin-user-timeout=.*/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
+# set Budgie as default session, otherwise login will fail
+sed -i 's/^#user-session=.*/user-session=budgie-desktop/' /etc/lightdm/lightdm.conf
+
+# set the default wallpaper
+mkdir -p /home/liveuser/.local/share/applications
+
+# Update installer icon
+sed -i -e 's/Icon=org.fedoraproject.AnacondaInstaller/Icon=drive-harddisk/' /usr/share/applications/liveinst.desktop
+
+# Show harddisk install on the desktop
+sed -i -e 's/NoDisplay=true/NoDisplay=false/' /usr/share/applications/liveinst.desktop
+mkdir /home/liveuser/Desktop
+cp /usr/share/applications/liveinst.desktop /home/liveuser/Desktop
+
+# and mark it as executable
+chmod +x /home/liveuser/Desktop/liveinst.desktop
+
+# allow anaconda to use system icon theme
+sed -i -e 's/settings.set_property("gtk-icon-theme-name", "Adwaita")//' /usr/lib64/python3.11/site-packages/pyanaconda/ui/gui/__init__.py
+
+# this goes at the end after all other changes.
+chown -R liveuser:liveuser /home/liveuser
+restorecon -R /home/liveuser
+EOF
+
+cp /etc/lightdm/lightdm.conf.d/50-ultramarine-lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+
---
+++ katsu/modules/liveimage/live.sh
@@ -0,0 +1,53 @@
+#!/bin/sh -x
+
+systemctl disable systemd-networkd-wait-online systemd-networkd systemd-networkd.socket
+
+# Enable livesys services
+systemctl enable livesys.service
+systemctl enable livesys-late.service
+
+# enable tmpfs for /tmp
+systemctl enable tmp.mount
+
+cat >> /etc/fstab << EOF
+vartmp   /var/tmp    tmpfs   defaults   0  0
+EOF
+
+# work around for poor key import UI in PackageKit
+rm -f /var/lib/rpm/__db*
+echo "Packages within this LiveCD"
+rpm -qa --qf '%{size}\t%{name}-%{version}-%{release}.%{arch}\n' |sort -rn
+# Note that running rpm recreates the rpm db files which aren't needed or wanted
+rm -f /var/lib/rpm/__db*
+
+
+# go ahead and pre-make the man -k cache (#455968)
+/usr/bin/mandb -c
+
+# remove random seed, the newly installed instance should make it's own
+rm -f /var/lib/systemd/random-seed
+
+# convince readahead not to collect
+# FIXME: for systemd
+
+echo 'File created by kickstart. See systemd-update-done.service(8).' \
+    | tee /etc/.updated >/var/.updated
+
+
+# Disable network service here, as doing it in the services line
+# fails due to RHBZ #1369794
+systemctl disable network
+systemctl disable systemd-networkd
+systemctl disable systemd-networkd-wait-online
+systemctl disable openvpn-client@\*.service
+systemctl disable openvpn-server@\*.service
+
+
+# Remove machine-id on pre generated images
+rm -f /etc/machine-id
+touch /etc/machine-id
+
+# make sure there aren't core files lying around
+rm -f /core*
+
+systemctl set-default graphical.target

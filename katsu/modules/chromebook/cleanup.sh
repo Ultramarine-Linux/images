f027198d90bf094fa2a8ce4d4a9d310b2e3349a0
Jaiden Riordan <jade@fyralabs.com>
2023-10-29 20:10:02 -0500

    configs

---
+++ katsu/modules/chromebook/cleanup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+systemctl disable systemd-networkd-wait-online systemd-networkd systemd-networkd.socket
+systemctl disable chronyd
+# systemctl disable polkit
+# Add polkitd user manually, weird hack because preinstall script doesn't run
+cat << EOF > /usr/lib/sysusers.d/extras.conf
+# This is a really weird hack
+#Type  Name    ID   Argument
+g      polkitd 114  
+u      polkitd 114:114 "User for polkitd" - -
+
+g      rpc     32
+u      rpc     32:32  "Rpcbind Daemon" - -
+EOF
+
+echo max_parallel_downloads=20 >> /etc/dnf/dnf.conf
+echo defaultyes=True >> /etc/dnf/dnf.conf
+
+systemd-sysusers
+
+

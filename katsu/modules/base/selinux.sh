e75c2bd12938287352d0cb948897fc104e75ed26
Lleyton Gray <lleyton@fyralabs.com>
2023-11-21 09:23:46 -0800

    Revert "fix: use fixfiles to set selinux labels"
    This reverts commit 05edc2a369e55f638869359d9a09b238f9421931.

--- katsu/modules/base/selinux.sh
+++ katsu/modules/base/selinux.sh
@@ -2,7 +2,5 @@
 
 echo "Setting up SELinux..."
 
-# setfiles -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts / || true
-# setfiles -F -e /proc -e /sys -e /dev /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true
-
-fixfiles -v -F -f relabel
+setfiles -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts / || true
+setfiles -F -e /proc -e /sys -e /dev /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true

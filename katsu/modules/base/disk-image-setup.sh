fdf0a9bd4d54be52e5ced6cf585a17f3c9eadbbc
lea <lleyton@fyralabs.com>
2024-05-22 17:20:55 -0700

    remove faulty disk-image-setup script

--- katsu/modules/base/disk-image-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-# force initial-setup to reconfigure the system
-touch /.unconfigured
-
-# delete the systemd files to force a fresh unconfigured state
-rm -vf /etc/{machine-id,localtime,hostname,shadow,locale.conf}

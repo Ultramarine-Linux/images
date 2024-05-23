c81988c0c68ca79b33ad85aa829100710ce39b66
Lleyton Gray <lleyton@fyralabs.com>
2024-05-23 14:51:59 -0700

    fix: make selinux work maybe (taken from mkosi)

--- katsu/modules/base/selinux.sh
+++ katsu/modules/base/selinux.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -euxo pipefail
 
 echo "Setting up SELinux..."
 
-setfiles -e /proc -e /sys /etc/selinux/targeted/contexts/files/file_contexts / || true
+BIN_POLICY=$(find /etc/selinux/targeted/policy/policy.* -maxdepth 1 | sort -Vr | head -1)
+
+setfiles -mF -c "${BIN_POLICY}" /etc/selinux/targeted/contexts/files/file_contexts /

9176aad4e45793bc02c17756b7d063e0a5bb0c81
Lleyton Gray <lleyton@fyralabs.com>
2024-05-23 00:28:51 -0700

    fix: use our panel for xfce live

--- katsu/modules/xfce/xfce-live.sh
+++ katsu/modules/xfce/xfce-live.sh
@@ -7,3 +7,8 @@ EOF
 
 # set livesys session type
 sed -i 's/^livesys_session=.*/livesys_session="xfce"/' /etc/sysconfig/livesys
+
+cat >>/var/lib/livesys/livesys-session-extra <<ALLEOF
+# Use our XFCE panel config
+cp /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /home/liveuser/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+ALLEOF

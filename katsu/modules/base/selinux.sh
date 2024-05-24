e2297cc6d51b8cbc9222312dc3acb31b8e67f0e1
lea <lleyton@fyralabs.com>
2024-05-24 00:25:50 -0700

    run in chroot... again

--- katsu/modules/base/selinux.sh
+++ katsu/modules/base/selinux.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 echo "Setting up SELinux..."
 
-BIN_POLICY=$(find "${CHROOT}"/etc/selinux/targeted/policy/policy.* -maxdepth 1 | sort -Vr | head -1)
+BIN_POLICY=$(find /etc/selinux/targeted/policy/policy.* -maxdepth 1 | sort -Vr | head -1)
 
-setfiles -m -F -p -r "${CHROOT}" -c "${BIN_POLICY}" "${CHROOT}"/etc/selinux/targeted/contexts/files/file_contexts "${CHROOT}"
+setfiles -m -F -p -c "${BIN_POLICY}" /etc/selinux/targeted/contexts/files/file_contexts /

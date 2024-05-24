2e156c398d19d422eac065e1ab97658adab4e693
lea <lleyton@fyralabs.com>
2024-05-24 00:54:58 -0700

    use original method of relabeling (god please work)

--- katsu/modules/base/selinux.sh
+++ katsu/modules/base/selinux.sh
@@ -4,6 +4,4 @@ set -euxo pipefail
 
 echo "Setting up SELinux..."
 
-BIN_POLICY=$(find /etc/selinux/targeted/policy/policy.* -maxdepth 1 | sort -Vr | head -1)
-
-setfiles -m -F -p -c "${BIN_POLICY}" /etc/selinux/targeted/contexts/files/file_contexts /
+setfiles -e /proc -e /sys /etc/selinux/targeted/contexts/files/file_contexts /

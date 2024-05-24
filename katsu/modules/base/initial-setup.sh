3423e5b6987fb9bedda81ca9d427d1dcbdc7f509
Cappy Ishihara <cappy@cappuchino.xyz>
2024-05-24 17:46:12 +0700

    fix: conditionally enable initial-setup based on existence of GIS

--- katsu/modules/base/initial-setup.sh
+++ katsu/modules/base/initial-setup.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash -x
 # Set up initial setup, might be redundant idk
-systemctl enable initial-setup
-touch /var/lib/gdm/run-initial-setup || true
-
+if rpm -q gnome-initial-setup; then
+    mkdir -p /var/lib/gdm
+    touch /var/lib/gdm/run-initial-setup
+else 
+    systemctl enable initial-setup || true
+fi
 
 # Set default target to graphical
 systemctl set-default graphical.target

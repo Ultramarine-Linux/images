e7fc5def5b5bab7f5065a04fcdd70f2a723a7cc6
lea <lleyton@fyralabs.com>
2024-05-26 12:16:27 -0700

    backport selinux fixes and use faster runners

--- katsu/modules/base/selinux.sh
+++ katsu/modules/base/selinux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
+set -euxo pipefail
+
 echo "Setting up SELinux..."
 
-setfiles -F -e /proc -e /sys -e /dev -e /bin /etc/selinux/targeted/contexts/files/file_contexts / || true
-setfiles -F -e /proc -e /sys -e /dev /etc/selinux/targeted/contexts/files/file_contexts.bin /bin || true
+setfiles -F -r "${CHROOT}" "${CHROOT}"/etc/selinux/targeted/contexts/files/file_contexts "${CHROOT}"
